8a8ee06f5bfd8bd15e14e368a9a47c9d7fcf062c
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-11-08 12:32:32 +0000

    Created the Spring project files for the  final project

---
+++ Crud-Character-Crushing/src/test/java/uk/jordanellis/runner/CrudCharacterCrushingApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.jordanellis.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCharacterCrushingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
