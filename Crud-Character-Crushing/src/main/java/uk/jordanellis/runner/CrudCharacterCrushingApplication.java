65354d5431e5ed47e720c14850b6560c4b997d3f
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-11-10 12:51:48 +0000

    DJC-23 #Close #Comment Implemented

---
+++ Crud-Character-Crushing/src/main/java/uk/jordanellis/runner/CrudCharacterCrushingApplication.java
@@ -0,0 +1,13 @@
+package uk.jordanellis.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCharacterCrushingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCharacterCrushingApplication.class, args);
+	}
+
+}
