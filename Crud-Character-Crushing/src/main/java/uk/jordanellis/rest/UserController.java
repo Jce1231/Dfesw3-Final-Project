898aa648d2b64c5b6529b59d3b15b883bd6bb977
Jordan Ellis <Jordan.ellis@hotmail.co.uk>
2021-11-10 15:12:59 +0000

    Fixed few things

--- Crud-Character-Crushing/src/main/java/uk/jordanellis/rest/UserController.java
+++ Crud-Character-Crushing/src/main/java/uk/jordanellis/rest/UserController.java
@@ -39,6 +39,13 @@ public class UserController {
 		return this.service.getUsers();
 	}
 
+	@GetMapping("/test")
+	public ResponseEntity<Users> test() {
+		Users testing = new Users("Jordan");
+		Users responseBody = this.service.createUsers(testing);
+		return new ResponseEntity<Users>(responseBody, HttpStatus.CONTINUE);
+	}
+
 	@PostMapping("/create")
 	public ResponseEntity<Users> createUser(@RequestBody Users newUser) {
 		Users responseBody = this.service.createUsers(newUser);
